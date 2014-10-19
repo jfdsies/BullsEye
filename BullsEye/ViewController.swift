4f2274be5e64758c8f8b6c5d7093d27139455de7
jfdsies <sora@soradeMac-mini.local>
2014-10-19 23:16:02 +0800

    看到p74

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,19 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello, World", message: "This is my first app!", preferredStyle: UIAlertControllerStyle.Alert)
+        let message = "The vlaue of the slider is: \(currentValue)" +
+                      "\nThe target value is: \(targetValue)"
         
-        let action = UIAlertAction(title: "Awesome", style: .Default, handler: nil)
+        let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
         
         alert.addAction(action)
         
         presentViewController(alert, animated: true, completion: nil)
+        startNewRound()
+        updateLabels()
+    }
+    
+    @IBAction func sliderMoved(slider: UISlider) {
+        currentValue = lroundf(slider.value)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,6 +47,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+    }
 
 }
 
